e8dd54180f8c6430c31ffc66edd644050f10562a
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-11 22:51:15 +0600

    feat:create register auth api

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,61 @@
+import bcrypt from "bcryptjs";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, type Request, type Response } from "express";
+import httpStatus from "http-status";
 import config from "./config";
-
+import { prisma } from "./lib/prisma";
 const app: Application = express();
 
-app.use(cors({ origin: config.app_url,
-  credentials:true
- }));
+app.use(cors({ origin: config.app_url, credentials: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+app.post("/api/auth/register", async (req: Request, res: Response) => {
+  const { name, email, password } = req.body;
+  const isUserExist = await prisma.user.findUnique({
+    where: { email },
+  });
+  if (isUserExist) {
+    res.status(httpStatus.CONFLICT).json({
+      success: false,
+      statusCode: httpStatus.CONFLICT,
+
+      message: "user already exited",
+    });
+  }
+  const hashpassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  const createUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashpassword,
+    },
+  });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createUser.id,
+      email: createUser.email || email,
+    },
+    omit: { password: true },
+  });
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode: httpStatus.CREATED,
+    data: { user },
+    message: "registation successfull",
+  });
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,14 @@ import { prisma } from "./lib/prisma";
 const port = config.port;
 async function main() {
   try {
+    await prisma.$connect();
     app.listen(port, () => {
-      
-        console.log(`server is running ${port}` )
+      console.log(`server is running ${port}`);
     });
-  } catch (error) {}
+  } catch (error) {
+    console.log("server error", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
 }
 main();
