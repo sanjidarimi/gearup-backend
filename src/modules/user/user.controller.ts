da66ebe5aad18b1fefbcdddbe4abc9ca6f92430f
sanjidarimi <sanjidarimi312@gmail.com>
2026-07-12 21:13:18 +0600

    refactor:make reused async func for response

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,20 +1,7 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+import { Request, Response } from "express";
 import httpStatus from "http-status";
+import { catchAsync } from "../../utils/CatchAsync";
 import { userService } from "./user.service";
-
-const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (error) {
-      res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-        success: false,
-        message:
-          error instanceof Error ? error.message : "Something went wrong",
-      });
-    }
-  };
-};
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const payload = req.body;
